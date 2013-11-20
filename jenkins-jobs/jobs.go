319d7e6825a665e7bb5ee6fe0282a400f27acc5c
Jonas Wiklund <jonas.wiklund@atex.com>
2013-11-20 09:53:37 +0100

    added ability to list config parameters

--- jenkins-jobs/jobs.go
+++ jenkins-jobs/jobs.go
@@ -25,9 +25,11 @@ func main() {
 		return
 	}
 	p := flag.String("pattern", "", "Pattern to restrict which jobs to report")
+	l := flag.Bool("list", false, "List key names for job")
+	la := flag.Bool("listall", false, "List key names for all jobs, not just the first that matches")
 	flag.Parse()
-	if len(flag.Args()) == 0 {
-		fmt.Println("No fields, fields to fetch argument is required")
+	if (len(flag.Args()) != 0 && *l) || (len(flag.Args()) == 0 && !*l) {
+		fmt.Println("Either specify fields to list or use -list to show field names")
 		return
 	}
 	var pattern *regexp.Regexp
@@ -48,9 +50,18 @@ func main() {
 			fmt.Printf("Could not fetch job %s due to %s", job, err.Error())
 			continue
 		}
-
-		for _, name := range flag.Args() {
-			fmt.Printf("%s\t%s\t%s\n", job.Name, name, cfg[name])
+		if *l {
+			for name, _ := range cfg {
+				fmt.Printf("%s\t%s\n", job.Name, name)
+			}
+			if !*la {
+				return
+			}
+		} else {
+			for _, name := range flag.Args() {
+				fmt.Printf("%s\t%s\t%s\n", job.Name, name, cfg[name])
+			}
 		}
+
 	}
 }
